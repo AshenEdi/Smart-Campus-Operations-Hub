adcdff033ed5f0dc01bbb03e3855fe06241841f0
Ashen Edward <ashen2003ml@gmail.com>
2026-03-09 13:35:06 +0530

    created file pages and components files(empty)

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,20 @@
+function Dashboard() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Smart Campus Operations Hub</h1>
+      <p>Welcome to the campus management system.</p>
+
+      <div>
+        <h2>Modules</h2>
+        <ul>
+          <li>Facilities & Assets</li>
+          <li>Booking Management</li>
+          <li>Maintenance Tickets</li>
+          <li>Notifications</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
